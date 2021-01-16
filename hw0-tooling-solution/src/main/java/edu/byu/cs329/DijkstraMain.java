f87ac75062259a44d9e6e888d95c82a86c8742f5
BuildTools <unconfigured@null.spigotmc.org>
2021-01-15 23:33:49 -0700

    part2

---
+++ hw0-tooling-solution/src/main/java/edu/byu/cs329/DijkstraMain.java
@@ -0,0 +1,15 @@
+
+
+public class DijkstraMain {
+
+	 public static void main(String[] args) {
+	    	final int[][] graph = { { 0, 2, 4, Dijkstra.M, Dijkstra.M, Dijkstra.M }, { 2, 0, 1, 4, Dijkstra.M, Dijkstra.M },
+	                { 4, 1, 0, 2, 6, Dijkstra.M }, { Dijkstra.M, 4, 2, 0, 1, 3 }, { Dijkstra.M, Dijkstra.M, 6, 1, 0, 5 },
+	                { Dijkstra.M, Dijkstra.M, Dijkstra.M, 3, 5, 0 } };
+	    	Dijkstra d = new Dijkstra(graph);
+	    
+			int sp = d.shortestPath(0, 4);
+	    	System.out.println("d = " + sp);
+		}
+
+}
