f2dd53580ebb680139855cd6cba2043e574436bb
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 21:40:22 -0700

    remove duplicates

--- src/main/java/edu/byu/cs329/DijkstraMain.java
+++ src/main/java/edu/byu/cs329/DijkstraMain.java
@@ -17,7 +17,6 @@ public class DijkstraMain {
       {4, 1, 0, 2, 6, Dijkstra.M}, {Dijkstra.M, 4, 2, 0, 1, 3}, 
       {Dijkstra.M, Dijkstra.M, 6, 1, 0, 5},
       {Dijkstra.M, Dijkstra.M, Dijkstra.M, 3, 5, 0 }};
-    logger.info("started the program");
     Dijkstra d = new Dijkstra(graph);
     int sp = d.shortestPath(0, 4);
     System.out.println("d = " + sp);
